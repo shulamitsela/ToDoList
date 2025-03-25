5134297d6d0307e9e0bbbd64360cc62e0b617b2c
shulamit <ss0556739224@gmail.com>
2025-03-25 13:27:49 +0200

    good shuli

---
+++ ToDoListReact-master/src/service.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+axios.defaults.baseURL = 'http://localhost:5087';
+
+const apiUrl = "http://localhost:5087"
+
+axios.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    console.error('שגיאה בבקשה:', error.response ? error.response.data : error.message);
+    return Promise.reject(error);
+  }
+);
+
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/getAll`) 
+    console.log(result);
+    return result.data;
+  },
+
+  addTask: async(name)=>{
+    const result = await axios.post(`/add?name=${encodeURIComponent(name)}`);    console.log('addTask', result)
+    return result.data;
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    const result = await axios.put(`/update/${id}`)
+    console.log('setCompleted', {id, isComplete})
+    console.log(result.data);    
+    return {};
+  },
+
+  deleteTask:async(id)=>{
+    const result = await axios.delete(`/delete/${id}`)
+    console.log('deleteTask')
+  }
+};
